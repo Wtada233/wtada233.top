fdf9d0596be8ad6fd82c4b0ee3f205b1b913e1e0
Wtada233 <wtada233@outlook.com>
2025-12-04 21:38:46 +0800

    init

--- src/i18n/translation.ts
+++ src/i18n/translation.ts
@@ -1,13 +1,8 @@
 import { siteConfig } from "../config";
 import type I18nKey from "./i18nKey";
 import { en } from "./languages/en";
-import { es } from "./languages/es";
-import { id } from "./languages/id";
 import { ja } from "./languages/ja";
 import { ko } from "./languages/ko";
-import { th } from "./languages/th";
-import { tr } from "./languages/tr";
-import { vi } from "./languages/vi";
 import { zh_CN } from "./languages/zh_CN";
 import { zh_TW } from "./languages/zh_TW";
 
@@ -18,7 +13,6 @@ export type Translation = {
 const defaultTranslation = en;
 
 const map: { [key: string]: Translation } = {
-	es: es,
 	en: en,
 	en_us: en,
 	en_gb: en,
@@ -29,13 +23,6 @@ const map: { [key: string]: Translation } = {
 	ja_jp: ja,
 	ko: ko,
 	ko_kr: ko,
-	th: th,
-	th_th: th,
-	vi: vi,
-	vi_vn: vi,
-	id: id,
-	tr: tr,
-	tr_tr: tr,
 };
 
 export function getTranslation(lang: string): Translation {
