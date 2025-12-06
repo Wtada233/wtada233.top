e444118e8fa955a30604ce7a59e1c9e5f9720fd5
Wtada233 <wtada233@outlook.com>
2025-12-06 15:33:11 +0800

    init

--- src/types/config.ts
+++ src/types/config.ts
@@ -40,6 +40,7 @@ export type SiteConfig = {
 	showLastModified?: boolean;
 
 	favicon: Favicon[];
+	toc: TOCConfig;
 };
 
 export type Favicon = {
@@ -106,3 +107,8 @@ export type BlogPostData = {
 export type ExpressiveCodeConfig = {
 	theme: string;
 };
+
+export interface TOCConfig {
+	enable: boolean;
+	depth: number;
+}
