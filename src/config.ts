068000f36400ecf199046c6d1f59e9f12c6064ab
Wtada233 <wtada233@outlook.com>
2025-12-07 21:06:44 +0800

    fix some bugs

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "_LFW_",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	keywords: "Wtada233, 折腾Linux, 技术分享, 博客搭建",
-	description: "Wtada233的个人博客，内容很杂。",
+	description: "Wtada233的个人博客，内容很杂。主要是折腾Linux相关。本站使用Fuwari搭建，欢迎在Github提交PR!",
 
 	themeColor: {
 		hue: 280, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
