3640757020f33208236954de667ce2ea8de5343f
Wtada233 <wtada233@outlook.com>
2025-12-19 21:10:32 +0800

    fix some bugs

---
+++ src/configs/friends.ts
@@ -0,0 +1,16 @@
+export const Friends = [
+	{
+		title: "白子的资源站",
+		imgurl: "https://nekowithu.top/favicon.ico",
+		desc: "刷机资源",
+		siteurl: "https://nekowithu.top",
+		tags: ["资源"],
+	},
+	{
+		title: "THW的博客",
+		imgurl: "https://image.tianhw.top/avatar.webp",
+		desc: "前途似海，来日方长",
+		siteurl: "https://tianhw.top",
+		tags: ["博客"],
+	},
+];
