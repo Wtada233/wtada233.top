dd6e2e8f5ec85f7370c790dfbaa6d607b4301896
Wtada233 <wtada233@outlook.com>
2025-12-23 21:10:44 +0800

    .

--- src/configs/friends.ts
+++ src/configs/friends.ts
@@ -1,14 +1,14 @@
 export const Friends = [
 	{
 		title: "白子的资源站",
-		imgurl: "https://nekowithu.top/favicon.ico",
+		imgurl: "/assets/friends/icons/nekowithu.ico",
 		desc: "刷机资源",
 		siteurl: "https://nekowithu.top",
 		tags: ["资源"],
 	},
 	{
 		title: "THW的博客",
-		imgurl: "https://image.tianhw.top/avatar.webp",
+		imgurl: "/assets/friends/icons/thw.webp",
 		desc: "前途似海，来日方长",
 		siteurl: "https://tianhw.top",
 		tags: ["博客"],
