8fe1a63f27fad056f7413ffc725f1a3e0e8bb99b
Wtada233 <wtada233@outlook.com>
2025-12-24 20:34:55 +0800

    .

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-export { siteConfig } from "./configs/site";
+export { expressiveCodeConfig } from "./configs/expressive-code";
+export { licenseConfig } from "./configs/license";
+export { musicPlayerConfig } from "./configs/music";
 export { navBarConfig } from "./configs/navbar";
 export { profileConfig } from "./configs/profile";
-export { licenseConfig } from "./configs/license";
-export { expressiveCodeConfig } from "./configs/expressive-code";
-export { musicPlayerConfig } from "./configs/music";
+export { siteConfig } from "./configs/site";
