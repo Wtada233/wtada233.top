22973200c11d539f0d941a5d5d45c9290eee49dd
Wtada233 <wtada233@outlook.com>
2025-12-25 19:49:19 +0800

    .

--- src/utils/content-utils.ts
+++ src/utils/content-utils.ts
@@ -245,3 +245,26 @@ export async function getBlogStats(): Promise<{
 
 	return statsCache;
 }
+
+interface PostCardData {
+	excerpt: string;
+	wordCount: string;
+	minuteCount: string;
+}
+
+export async function getPostCardData(entry: CollectionEntry<"posts">): Promise<PostCardData> {
+	const { remarkPluginFrontmatter } = await entry.render();
+
+	const excerpt = remarkPluginFrontmatter.excerpt;
+	const words = remarkPluginFrontmatter.words;
+	const minutes = remarkPluginFrontmatter.minutes;
+
+	const wordCountString = `${words} ${i18n(words === 1 ? I18nKey.wordCount : I18nKey.wordsCount)}`;
+	const minuteCountString = `${minutes} ${i18n(minutes === 1 ? I18nKey.minuteCount : I18nKey.minutesCount)}`;
+
+	return {
+		excerpt,
+		wordCount: wordCountString,
+		minuteCount: minuteCountString,
+	};
+}
